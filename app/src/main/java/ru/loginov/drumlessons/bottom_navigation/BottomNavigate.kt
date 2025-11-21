e17aaa8bc108ddd79592380152f891e7898a5c87
LginvM <84954335+LginvM@users.noreply.github.com>
2025-11-21 22:18:44 +0600

    navigation Bar logic

--- app/src/main/java/ru/loginov/drumlessons/bottom_navigation/BottomNavigate.kt
+++ app/src/main/java/ru/loginov/drumlessons/bottom_navigation/BottomNavigate.kt
@@ -1,11 +1,19 @@
 package ru.loginov.drumlessons.bottom_navigation
 
 import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import ru.loginov.drumlessons.LoginScreen
+
 
 @Composable
 fun BottomNavigate (
@@ -23,6 +31,31 @@ fun BottomNavigate (
         backgroundColor = Color.White
     ){
         val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRout = backStackEntry?.destination?.route
+        listItems.forEach {
+            item ->
+            BottomNavigationItem(
+                selected = currentRout == item.route,
+                onClick = {
+                    navController.navigate(item.route)
+                },
+                icon = {
+                    Icon(painter = painterResource(id = item.iconId), contentDescription = )
+                },
+                label = {
+                    Text(
+                        text = item.title,fontSize = 9.sp
+                    )
+                },
+                selectedContentColor = Color.Red,
+                unselectedContentColor = Color.Gray
+            )
+        }
     }
 }
 
+@Composable
+@Preview(showBackground = true)
+private fun LoginPreview(){
+    BottomNavigate(navController = { } as NavController)
+}
